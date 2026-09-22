f7b0029b58ce832a3a9d0816f17ada46d404309c
Muskan Shaik <muskanshaik.baji@gmail.com>
2026-09-22 11:47:21 +0530

    Configure backend for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
